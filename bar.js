4a40d1d807b662ec8f42ded47ab0a2e9bfabd5b0
karmanya007 <karmanyasharma5@gmail.com>
2020-07-28 23:08:59 +0530

    Second progress bar added

--- bar.js
+++ bar.js
@@ -1,27 +1,33 @@
 (function () {
-	//const pkg_size = 100; // In MB
-	//const net_speed = 1000; // In Mb/s
 	let init = true;
+	const incrementVal = 0.09;
 	const start = document.querySelector('.start');
 	start.addEventListener('click', startJs);
-	const bar = document.querySelector('.bar');
+	const bar1 = document.querySelector('.bar1');
+	const bar2 = document.querySelector('.bar2');
 	const percentage = document.querySelector('.percentage');
 	function startJs () {
 		if (init) {
+			bar2.style.setProperty('--width', 0);
 			start.classList.add('startActive');
 			init = !init;
-			let width = 0;
-			const id = setInterval(setFrame, /* pkg_size / (net_speed / 8) * 1000 / 100 */ 1);
+			let width1 = 0;
+			const id = setInterval(setFrame, 1);
 			function setFrame () {
-				if (width >= 100) {
+				// For first bar
+				if (width1 >= 100) {
 					clearInterval(id);
 					start.classList.remove('startActive');
 					init = true;
 				} else {
-					width += 0.09;
-					percentage.innerText = Math.floor(width) + '%';
-					bar.style.width = width + '%';
+					width1 += incrementVal;
+					percentage.innerText = Math.floor(width1) + '%';
+					bar1.style.width = width1 + '%';
 				}
+				// For second bar
+				const computedStyle = getComputedStyle(bar2);
+				const width2 = parseFloat(computedStyle.getPropertyValue('--width')) || 0;
+				bar2.style.setProperty('--width', width2 + incrementVal);
 			}
 		}
 	}
